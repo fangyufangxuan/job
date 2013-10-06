ee1da348b2dce97683acdc60d5ec636add6473ea
Shuai Fan <shuai900217@126.com>
2013-10-07 00:00:09 +0800

    Makefile: Add automake helloworld

---
+++ programmer/c_cpp/makefile/automake_example/src/main.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  main
+ *
+ *        Version:  1.0
+ *        Created:  10/06/2013 10:55:03 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  shuai fan,
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <config.h>
+#include <stdio.h>
+
+int main(void)
+{
+    puts("hello world!");
+    puts("This is " PACKAGE_STRING ".");
+
+    return 0;
+}
